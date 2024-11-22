ebaff84f5e98d5ae19cb652167346144637dc171
krishnaju07 <krishnamahi0703@gmail.com>
2024-11-23 01:43:31 +0530

    hhh

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route index element={<Navigate to="/" replace />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="appointments" element={<Appointments />} />
             <Route path="billing" element={<Billing />} />
